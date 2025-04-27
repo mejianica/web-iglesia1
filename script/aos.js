a7e735bb5a06d2c825c7e80371ac03fb915f5e08
mejianica <mejianica@hotmail.com>
2025-04-27 10:35:25 -0600

    se agrego script aos

---
+++ script/aos.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init({
+      duration: 900, // Duración de la animación en milisegundos
+      easing: 'ease-in-out', // Tipo de easing (transición)
+      once: false, // Animar solo la primera vez
+      mirror: true, // Permite que la animación ocurra al volver a hacer 
+    });
+
+  });
+  
