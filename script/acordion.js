f43802b6a20f74a825c73a5137592b94ab0326f3
Ronaldmera <ronaldalme1230@gmail.com>
2025-04-28 13:42:40 -0500

    seccion acordeon responsive v1

--- script/acordion.js
+++ script/acordion.js
@@ -1,26 +1,30 @@
-// Control del acordeón
 const headers = document.querySelectorAll('.accordion-header');
 
 headers.forEach(header => {
   header.addEventListener('click', () => {
     const content = header.nextElementSibling;
     const isOpen = content.classList.contains('open');
-    
-   header.classList.toggle('active');
+
+    header.classList.toggle('active');
 
     // Cerrar todos
     document.querySelectorAll('.accordion-content').forEach(c => {
       c.style.maxHeight = null;
+      c.style.paddingTop = '0px';
+      c.style.paddingBottom = '0px';
       c.classList.remove('open');
     });
 
     // Abrir el clickeado si estaba cerrado
     if (!isOpen) {
-      content.style.maxHeight = content.scrollHeight + 'px';
       content.classList.add('open');
+      content.style.maxHeight = content.scrollHeight + 'px';
+      content.style.paddingTop = '30px';
+      content.style.paddingBottom = '30px';
     }
   });
 });
+
 
 // Botones "Ver más"
 // const buttons = document.querySelectorAll('.btn-vermas');
